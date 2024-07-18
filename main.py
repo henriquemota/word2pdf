ed9120df184ee7248e3548e8b0e11abffb647cd0
henriquemota <mota.henrique@gmail.com>
2024-07-18 04:32:57 -0300

    coverte arquivos do word para PDF

---
+++ main.py
@@ -0,0 +1,13 @@
+import fnmatch
+from os import getcwd, path, walk
+
+from docx2pdf import convert
+
+root_folder = path.join(getcwd(), 'arquivos')
+
+for root, dirnames, files in  walk(root_folder): 
+	for filename in fnmatch.filter(files, '*.docx'):
+		convert(path.join(root, filename))
+		print(f"arquivo {root}{filename} convertido")
+
+print("Conversão realizada sucesso.")
