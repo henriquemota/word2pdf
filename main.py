7a0c4b7345f17f8df853c2e8519f5283394fad31
henriquemota <mota.henrique@gmail.com>
2024-07-18 04:43:30 -0300

    implementacao do parametro do keep ative

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ root_folder = path.join(getcwd(), 'arquivos')
 
 for root, dirnames, files in  walk(root_folder): 
 	for filename in fnmatch.filter(files, '*.docx'):
-		convert(path.join(root, filename))
+		convert(path.join(root, filename), keep_active=False)
 		print(f"arquivo {root}{filename} convertido")
 
 print("Conversão realizada sucesso.")
